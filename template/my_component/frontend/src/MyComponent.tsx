d9c6e2b80c4c729e43ec7f714bd22389e405f1a3
karlotimmerman <karlo.timmerman@skydust.io>
2023-01-15 20:57:56 +0100

    update config

--- template/my_component/frontend/src/MyComponent.tsx
+++ template/my_component/frontend/src/MyComponent.tsx
@@ -14,34 +14,7 @@ interface State {
  * This is a React-based component template. The `render()` function is called
  * automatically when your component should be re-rendered.
  */
-class MyComponent extends StreamlitComponentBase<State> {
-  public state = { numClicks: 0, isFocused: false }
-
-  public render = (): ReactNode => {
-    // Arguments that are passed to the plugin in Python are accessible
-    // via `this.props.args`. Here, we access the "name" arg.
-    const name = this.props.args["name"]
-
-    // Streamlit sends us a theme object via props that we can use to ensure
-    // that our component has visuals that match the active theme in a
-    // streamlit app.
-    const { theme } = this.props
-    const style: React.CSSProperties = {}
-
-    // Maintain compatibility with older versions of Streamlit that don't send
-    // a theme object.
-    if (theme) {
-      // Use the theme object to style our button border. Alternatively, the
-      // theme style is defined in CSS vars.
-      const borderStyling = `1px solid ${
-        this.state.isFocused ? theme.primaryColor : "gray"
-      }`
-      style.border = borderStyling
-      style.outline = borderStyling
-    }
-
-    // Show a button and some text.
-    // When the button is clicked, we'll increment our "numClicks" state
+class MyComponent
     // variable, and send its new value back to Streamlit, where it'll
     // be available to the Python program.
     return (
